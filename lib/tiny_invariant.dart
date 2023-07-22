0a7e3fb6c49c8c2fea54b635fdd8d61ebc21f2a5
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-07-22 12:25:45 -0400

    Update tiny_invariant.dart

--- lib/tiny_invariant.dart
+++ lib/tiny_invariant.dart
@@ -1,17 +1,19 @@
 library tiny_invariant
 
-const bool isProduction = bool.fromEnvironment('dart.vm.product');
-const String prefix = 'Invariant failed';
+const bool _isProduction = bool.fromEnvironment('dart.vm.product');
+const String _prefix = 'Invariant failed';
 
-void invariant(bool condition, [String Function() message]) {
-  if (condition) {
-    return;
+void invariant(bool condition, String message) {
+  assert(() {
+    if (!condition) {
+      final errorMessage = message != null ? '$_prefix: $message' : _prefix;
+      throw AssertionError(errorMessage);
+    }
+    return true;
+  }(), '');
+  
+  if (!_isProduction && message != null) {
+    final errorMessage = '$_prefix: $message';
+    throw AssertionError(errorMessage);
   }
-
-  if (isProduction) {
-    throw Error(prefix);
-  }
-
-  final provided = message?.call();
-  final value = provided != null ? '$prefix: $provided' : prefix;
-  throw Error(value);
+}
