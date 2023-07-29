8bde2d02402103a8a33d18d1c54976e41809713a
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-07-28 21:56:33 -0400

    Update tiny_invariant.dart

--- lib/tiny_invariant.dart
+++ lib/tiny_invariant.dart
@@ -1,18 +1,14 @@
 library tiny_invariant;
 
-import 'package:dotenv/dotenv.dart' as dotenv;
-
 const String _prefix = 'Invariant failed';
 
-void invariant(bool condition, [String message]) {
-  final isProduction = dotenv.env['APP_ENV'] == 'production';
-  
-  if (!condition) {
-    final errorMessage = message != null ? '$_prefix: $message' : _prefix;
-    if (isProduction) {
-      throw Error(errorMessage);
-    } else {
-      throw AssertionError(errorMessage);
-    }
+void invariant(bool condition, String? message) {
+  final msg = '$_prefix: $message';
+  if (condition) {
+    return;
+  } else if (message!.isNotEmpty) {
+    throw Exception(msg);
+  } else {
+    throw Exception(_prefix);
   }
 }
